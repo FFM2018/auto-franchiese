e6a079c275aef213895ed782deda56b1abad8ef8
FFM2018 <1felipefmachado@gmail.com>
2024-02-01 18:17:09 -0300

    projects were created

---
+++ auto-franchiese-api/src/main/java/com/autofranchieseapi/AutoFranchieseApiApplication.java
@@ -0,0 +1,13 @@
+package com.autofranchieseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFranchieseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoFranchieseApiApplication.class, args);
+	}
+
+}
---
+++ auto-franchiese-api/src/test/java/com/autofranchieseapi/AutoFranchieseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autofranchieseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoFranchieseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
